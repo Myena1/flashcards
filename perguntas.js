aa210071345c999412230e79a94a0404732957ec
Myena1 <00001111675946sp@al.educacao.sp.gov.br>
2024-11-17 18:47:39 -0300

    Flashcards

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    'Matemática',
+    'Quanto e 1+1?',
+    '1+1 é igual a 2'
+)
+
+criaCartao(
+    'Artes',
+    'Misturar azul e amarelo dá que cor?',
+    'Verde'
+)
+
+criaCartao(
+    'História',
+    'Quem foi eleito presidente do Brasil nas eleições de 2022?',
+    'Luis Inácio Lula da Silva'
+)
+
+criaCartao(
+    'Música',
+    'Qual principal gênero musical no Brasil?',
+    'A MPB(Mísica Popular Brasileira)'
+)
+
+
+criaCartao(
+    'História',
+    'Quando e o dia da conciência negra?',
+    'Dia 20 de novembro'
+)
+
+criaCartao(
+    'História',
+    'Quando e o dia do Saci?',
+    'Dia 31 de Outubro'
+)
